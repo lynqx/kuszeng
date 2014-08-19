57c7f447647aef473d3eb2fae73cf26e43a35da8
Lynqx <aakinjiola@gmail.com>
2014-08-19 12:31:34 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Kuszeng Home and Securities</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/jquery.js"></script>
+<!--  STEP ONE: insert path to SWFObject JavaScript -->
+<script type="text/javascript" src="js/swfobject/swfobject.js"></script>
+
+<!--  STEP TWO: configure SWFObject JavaScript and embed CU3ER slider -->
+<script type="text/javascript">
+		var flashvars = {};
+		flashvars.xml = "config.xml";
+		flashvars.font = "font.swf";
+		var attributes = {};
+		attributes.wmode = "transparent";
+		attributes.id = "slider";
+		swfobject.embedSWF("cu3er.swf", "cu3er-container", "960", "316", "9", "expressInstall.swf", flashvars, attributes);
+</script>
+<style type="text/css">
+<!--
+#cu3er-container {width:960px; outline:0;}
+-->
+</style>
+</head>
+<body>
+<div class="main">
+  <div class="blok_header">
+    <div class="header">
+      <div class="logo"><a href="index.html"><img src="images/logo.gif" width="226" height="154" border="0" alt="logo" /></a></div>
+      <div class="search">
+        <form id="form1" name="form1" method="post" action="">
+          <label><span>
+            <input name="q" type="text" class="keywords" id="textfield" maxlength="50" value="Search..." />
+            </span>
+            <input name="b" type="image" src="images/search.gif" class="button" />
+          </label>
+        </form>
+      </div>
+      <div class="clr"></div>
+      <div class="menu_resize">
+        <div class="menu">
+          <ul>
+            <li><a href="index.html" class="active">Home</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="portfolio.html">Portfolio</a></li>
+            <li><a href="contact.html">Contact us</a></li>
+          </ul>
+        </div>
+        <div class="clr"></div>
+      </div>
+      <div class="clr"></div>
+    </div>
+    <div class="clr"></div>
+  </div>
+  <div class="clr"></div>
+  <div class="header_text_bg">
+    <div class="header_text">
+      <!--  STEP THREE: insert CU3ER div container -->
+      <div id="cu3er-container"> <a href="http://www.adobe.com/go/getflashplayer"> <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /> </a> </div>
+      <div class="clr"></div>
+    </div>
+    <div class="clr"></div>
+  </div>
+  <div class="top_text">
+    <div class="block">
+      <h2>Web Development</h2>
+      <img src="images/top_img_1.gif" alt="picture" />
+      <p>Fusce vehicula dignissim ligula. <br />
+        Vestibulum sit amet neque eu neque suscipit consequat quis vel risus. </p>
+      <p><a href="#">Read More »</a></p>
+    </div>
+    <div class="block">
+      <h2>Quality Products</h2>
+      <img src="images/top_img_2.gif" alt="picture" />
+      <p>Fusce vehicula dignissim ligula. <br />
+        Vestibulum sit amet neque eu neque suscipit consequat quis vel risus. </p>
+      <p><a href="#">Read More »</a></p>
+    </div>
+    <div class="block">
+      <h2>Best Business Planst</h2>
+      <img src="images/top_img_3.gif" alt="picture" />
+      <p>Fusce vehicula dignissim ligula. <br />
+        Vestibulum sit amet neque eu neque suscipit consequat quis vel risus. </p>
+      <p><a href="#">Read More »</a></p>
+    </div>
+    <div class="clr"></div>
+  </div>
+  <div class="body_resize">
+    <div class="body">
+      <div class="body_big">
+        <h2>Welcome to company.<br />
+          <span>Praesent laoreet ornare odioi porta aeanen leo justo varius</span></h2>
+        <p> <strong>Sed quis libero massa, vel sagittis augue. </strong><br />
+          Phasellus iaculis varius est at laoreet. Praesent laoreet ornare odio id porta. Aenean quis leo justo, sit amet varius purus. Ras vitae enim nulla. Aenean tristique eros in mauris porta id suscipit libero scelerisque. Sollicitudin vel sollicitudin qui facilisis non massaNam id jus nisl ullamcorper laoreet nec ut diam. Nam eget nisi dui, vita hendrerit risus. Nunc non mas neque, at rhoncus nulla.Vivamus in metus nisi, ut gravida dolor.</p>
+        <p>Pellentesque fermentum aliquam interdum. Mauris ligula dui, gravida nec tempus mollis, tempor at odio. Ut porttitor, orci sagittis pretium varius, felis sapien pharetra eros, quis fermentum dui ligula eget justo. </p>
+        <div class="bg"></div>
+      </div>
+      <div class="body_small">
+        <h2>Latest projects.<br />
+          <span> Lorem ipsum dolor sit amet</span></h2>
+        <img src="images/last_1.gif" alt="picture" class="port" /> <img src="images/last_2.gif" alt="picture" class="port"/> <img src="images/last_3.gif" alt="picture" class="port"/> <img src="images/last_4.gif" alt="picture" class="port" /> <img src="images/last_5.gif" alt="picture" class="port"  /> <img src="images/last_6.gif" alt="picture" class="port" /> <img src="images/last_7.gif" alt="picture" class="port" /> <img src="images/last_8.gif" alt="picture" class="port" />
+        <div class="bg"></div>
+        <p><em>&quot;Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse voluptas nulla pariatur?&quot;</em></p>
+        <p style="float:right;"><strong>-John Smith, <em>webdesigner</em></strong></p>
+      </div>
+      <div class="clr"></div>
+    </div>
+    <div class="clr"></div>
+  </div>
+</div>
+<div class="footer">
+  <div class="footer_resize"><a href="#"><img src="images/footer_logo.gif" alt="picture" width="227" height="84" border="0" /></a>
+    <p class="leftt">© Copyright YourSiteName. All Rights Reserved<br />
+      Home | Contact | RSS  (DT) <a href="http://www.dreamtemplate.com">Website Templates</a></p>
+    <div class="clr"></div>
+  </div>
+  <div class="clr"></div>
+</div>
+</body>
+</html>
